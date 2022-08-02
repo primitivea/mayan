c8a1fcc54400aa622cab4f42bf0fcf7c0644865e
primitivea <94339947+primitivea@users.noreply.github.com>
2022-08-02 11:00:12 +0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -40,7 +40,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Trending Tutorial'
+project = u'IG Fonts Tutorial'
 copyright = u'2022, Techidn Team'
 
 # The version info for the project you're documenting, acts as replacement for
